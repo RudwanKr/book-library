59bc32a72630d6d38661946304fdad21e3843c97
RudwanKr <rudwan20khmer03@gmail.com>
2025-09-06 15:08:28 +0200

    book card

--- src/app/components/book-card/book-card.ts
+++ src/app/components/book-card/book-card.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 
 @Component({
   selector: 'app-book-card',
@@ -7,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './book-card.scss'
 })
 export class BookCard {
+  @Input() book :any; // receives book data from parent (HomeComponent)
 
+  getCoverUrl(): string {
+    if (this.book.cover_i) {
+      // OpenLibrary cover service
+      return `https://covers.openlibrary.org/b/id/${this.book.cover_i}-M.jpg`;
+    }
+
+    // fallback image if no cover
+    return 'https://via.placeholder.com/150x220?text=No+Cover';
+  }
 }
