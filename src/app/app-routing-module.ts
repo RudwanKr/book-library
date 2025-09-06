4dbd341590de0fd1616ad321d5ecd7b2988b3cdb
RudwanKr <rudwan20khmer03@gmail.com>
2025-09-06 14:15:46 +0200

    routing

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { BookDetails } from './pages/book-details/book-details';
+import { Favorites } from './pages/favorites/favorites';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: Home},
+  { path: 'book/:id', component: BookDetails},
+  { path: 'favorites', component: Favorites},
+  { path: '**', redirectTo: ''},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
