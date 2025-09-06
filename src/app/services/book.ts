6ff1bd83977c39ab192d4fcc89020169c310df30
RudwanKr <rudwan20khmer03@gmail.com>
2025-09-06 14:21:15 +0200

    implement service

--- src/app/services/book.ts
+++ src/app/services/book.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Book {
-  
+  private apiUrl = 'https://openlibrary.org';
+
+  constructor(private http: HttpClient) {}
+
+  searchBook(query: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/search.json?q=${query}`);
+  }
+
+  getBookDetails(olid: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/works/${olid}.json`);
+  }
 }
