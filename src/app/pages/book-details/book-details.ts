57c71ff87d067a9596a44c694227cfeb775d2dc8
RudwanKr <rudwan20khmer03@gmail.com>
2025-09-06 15:25:08 +0200

    book details

--- src/app/pages/book-details/book-details.ts
+++ src/app/pages/book-details/book-details.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Book } from '../../services/book';
 
 @Component({
   selector: 'app-book-details',
@@ -7,5 +9,42 @@ import { Component } from '@angular/core';
   styleUrl: './book-details.scss'
 })
 export class BookDetails {
+  bookId: string = '';
+  book: any = null;
+  loading: boolean = true;
+  error: string = '';
 
+  constructor(private route: ActivatedRoute, private bookService: Book) {}
+
+  ngOnInit(): void {
+    this.bookId = this.route.snapshot.paramMap.get('id') || '';
+
+    if(this.bookId) {
+      this.fetchBookDetails()
+    } else {
+      this.error = 'No book ID provided.';
+      this.loading = false;
+    }
+  }
+
+  fetchBookDetails() {
+    this.bookService.getBookDetails(this.bookId).subscribe({
+      next: (res) => {
+        this.book = res;
+        this.loading = false;
+      },
+      error: ()=> {
+        this.error = 'Failed to load book details';
+        this.loading = false;
+      }
+    })
+  }
+
+  getCoverUrl(): string {
+    if(this.book && this.book.covers?.length) {
+      return `https://covers.openlibrary.org/b/id/${this.book.covers[0]}-L.jpg`;
+    }
+
+    return 'https://via.placeholder.com/200x300?text=No+Cover';
+  }
 }
